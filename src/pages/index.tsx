26b33d46bbda06c20e429b0ba0314cdd0c34317b
Cairo Campos <45980831+cairocampos@users.noreply.github.com>
2023-08-12 11:24:43 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <h1>Letícia, eu amo você. Se você me ama, dá uma risadinha! 😍</h1>
+  return <h1>Letícia, eu amo você. Se você me ama, dá uma risadinha! 😍</h1>;
 }
