4a49fb21c026348d6dabf64a6399ce611531c341
Cairo Campos <45980831+cairocampos@users.noreply.github.com>
2023-12-22 13:54:20 +0000

    add `database.js` and `.env` files

--- src/pages/api/v1/status/index.ts
+++ src/pages/api/v1/status/index.ts
@@ -1,6 +1,10 @@
+import database from "../../../../infra/database";
+
 import { NextApiResponse, NextApiRequest } from "next";
 
-function status(request: NextApiRequest, response: NextApiResponse) {
+async function status(request: NextApiRequest, response: NextApiResponse) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({
     chave: "Valor",
   });
