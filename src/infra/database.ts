d7acf267761f49031e291a9edf5cc27964bfcb79
Cairo Campos <45980831+cairocampos@users.noreply.github.com>
2024-02-26 13:34:12 +0000

    makes `database.js` `client.connect()` more robust to error

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -8,17 +8,16 @@ async function query(queryObject: any) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
-
-  return result;
 }
 
 export default {
